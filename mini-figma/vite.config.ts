77eadd4b921d0129a70119e6c1ed75490b1620ef
rottenDox22 <busik7058@gmail.com>
2026-09-23 22:54:57 +0300

    feat: deploy mini-figma to GitHub Pages via Actions

--- mini-figma/vite.config.ts
+++ mini-figma/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/mini_sigma_pro_mogger/',
   plugins: [react(), tailwindcss()],
 });
